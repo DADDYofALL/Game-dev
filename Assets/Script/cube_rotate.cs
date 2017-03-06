0f2dcdf42b38bec966966b5d576605ee4cbcd6c9
DADDYofALL <nutchanon.r@ku.th>
2017-03-06 15:46:22 +0700

    Add cube and some material

---
+++ Assets/Script/cube_rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_rotate : MonoBehaviour {
+
+	private float st;
+
+	void Start (){
+		st = Time.time;	
+	}
+
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		if (Time.time - st > 5){
+			Destroy (this.gameObject);
+		}
+	}
+}
